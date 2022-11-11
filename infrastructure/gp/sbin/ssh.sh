f3fa5e4e4b4ed7b2df91ebea81d1c355100bf838
lonmstalker <nkochnev@zuzex.com>
2022-11-11 17:29:08 +0300

    some fixes

--- infrastructure/gp/sbin/ssh.sh
+++ infrastructure/gp/sbin/ssh.sh
@@ -2,8 +2,6 @@
 echo "${BUILD_USER}" | sudo -S service ssh start
 
 mkdir -p "$HOME"/.ssh &&
-  mkdir "$HOME"/.ssh/authorized_keys &&
-  ssh-keygen -t rsa -b 4096 -P "" -f "$HOME"/.ssh/id_rsa &&
-  ssh-keygen -R sdw1
+  ssh-keygen -t rsa -b 4096 -P "" -f "$HOME"/.ssh/id_rsa
 
 sleep infinity
--- infrastructure/gp/sbin/start-master.sh
+++ infrastructure/gp/sbin/start-master.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "${BUILD_USER}" | ssh-copy-id -i "$HOME"/.ssh/id_rsa -p 22 gpadmin@sdw1
+echo "${BUILD_USER}" | ssh-copy-id -i "$HOME"/.ssh/id_rsa -p 22 gpadmin@mdw
+echo "${BUILD_USER}" | ssh-copy-id -i "$HOME"/.ssh/id_rsa -p 22 sdw1
 
 echo "${BUILD_USER}" | sudo -S chmod 700 "$HOME"/.ssh &&
   echo "${BUILD_USER}" | sudo -S chmod 700 "$HOME"/.ssh/id_rsa &&
@@ -12,9 +13,9 @@ mkdir -p "$HOME"/data/master/global/pg_control
 . /usr/local/greenplum-db-"$GREENPLUM_VERSION"/greenplum_path.sh
 
 echo "current greenplum nodes:" &&
-  cat "$HOME"/hostfile_exkeys &&
+  cat "$HOME"/etc/hostfile_exkeys &&
   echo
 
-gpssh -f "$HOME"/hostfile_exkeys -e "ls -l /usr/local/greenplum-db-$GREENPLUM_VERSION"
+gpssh -f "$HOME"/etc/hostfile_exkeys -e "ls -l /usr/local/greenplum-db-$GREENPLUM_VERSION"
 
-gpinitsystem -c "$HOME"/gpinitsystem_config -h "$HOME"/hostfile_gpssh_segonly -P 88 -p "$HOME"/data/master/postgresql.conf
+gpinitsystem -c "$HOME"/etc/gpinitsystem_config -h "$HOME"/etc/hostfile_gpssh_segonly -P 88 -p "$HOME"/data/master/postgresql.conf
